b06a612191c9c9c574a84353f213f99f62f402f4
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2021-11-09 23:04:22 +0100

    Card list component created

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "asc1",
-        },
-        {
-          name: "Dracula",
-          id: "asc2",
-        },
-        {
-          name: "Zombie",
-          id: "asc3",
-        },
-      ],
+      monsters: [],
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList name="Nelson">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
